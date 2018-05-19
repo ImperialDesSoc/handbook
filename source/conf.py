7bb18ad100288d662c891433784ee83a124f42d5
nebbles <bsg115@ic.ac.uk>
2018-05-19 18:31:18 +0100

    remove style override completely

--- source/conf.py
+++ source/conf.py
@@ -97,11 +97,11 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/style_override.css',
-        ],
-     }
+# html_context = {
+#     'css_files': [
+#         '_static/style_override.css',
+#         ],
+#      }
 
 html_favicon = '_static/favicon.png'
 html_logo = '_static/logo.png'
